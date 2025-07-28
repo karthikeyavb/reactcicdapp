81eda3b1eb0be5bda77341d6f3d0d5869a982412
karthikeyavb <bikted29@gmail.com>
2025-07-28 12:19:38 +0530

    Revert "deploy into git pages"
    This reverts commit b295c037849036789ba46bf47863b8e0bbed8f00.
    
    git push origin main
    git push origin main

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <>
       <div>
         <a href="https://vite.dev" target="_blank">
-          <img src={reactLogo} className="logo" alt="Vite logo" />
+          <img src={reactlogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
